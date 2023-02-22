35871ab083022b8facaa9991d624e523bffc96dd
Utkarsh Goel <ugoel911@gmail.com>
2023-02-22 14:08:12 +0530

    navbar and footer added

---
+++ src/components/Navbar/items.js
@@ -0,0 +1,24 @@
+const items = [
+  <div to={"/about"} className="navbar__nav-item">
+    About
+  </div>,
+  <div to={"/branches"} className="navbar__nav-item">
+    Branches
+  </div>,
+  <div to={"/team"} className="navbar__nav-item">
+    Team
+  </div>,
+  <div to={"/Events"} className="navbar__nav-item">
+    Events
+  </div>,
+  <div
+    style={{
+      color: "#0075FF",
+    }}
+    className="navbar__nav-item"
+  >
+    Login
+  </div>,
+];
+
+export default items;
