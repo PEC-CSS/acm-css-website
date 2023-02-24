87d16c000c9555b01df33fdc1c273d6d9f7347d0
Utkarsh Goel <ugoel911@gmail.com>
2023-02-25 01:04:52 +0530

    added animation on navbar link hover

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,48 @@ import "./Navbar.css";
 
 import items from "./items";
 
+const navLinkAnimation = (e) => {
+  const currentChild = e.target.childNodes[0];
+  const originalHeader = currentChild.dataset.original;
+
+  if (originalHeader === "Login") return;
+
+  const getRandomCharacter = () => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return characters[Math.floor(Math.random() * 26)];
+  };
+
+  let start = 0;
+  const interval = setInterval(() => {
+    try {
+      currentChild.innerText = currentChild.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < start) return originalHeader[index];
+          else if (index < 1) return getRandomCharacter();
+          else return getRandomCharacter().toLowerCase();
+        })
+        .join("");
+
+      if (start >= originalHeader.length) {
+        clearInterval(interval);
+        currentChild.innerText = originalHeader;
+      }
+
+      start += 1 / 2;
+    } catch (e) {
+      currentChild.innerText = originalHeader;
+    }
+  }, 30);
+};
+
+const returnLinkAnimation = (e) => {
+  try {
+    const currentChild = e.target.childNodes[0];
+    currentChild.innerText = currentChild.dataset.original;
+  } catch (e) {}
+};
+
 const NavigationMenu = () => {
   return (
     <Navbar collapseOnSelect expand="lg">
@@ -15,7 +57,13 @@ const NavigationMenu = () => {
           <Nav className="me-auto"></Nav>
           <Nav>
             {items.map((item, key) => (
-              <Nav.Link key={key}>{item}</Nav.Link>
+              <Nav.Link
+                key={key}
+                onMouseEnter={navLinkAnimation}
+                onMouseLeave={returnLinkAnimation}
+              >
+                {item}
+              </Nav.Link>
             ))}
           </Nav>
         </Navbar.Collapse>
