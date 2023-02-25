f736acd13be17576c3e8df998dd349afac2bae2e
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2023-02-25 17:42:47 +0530

    updated homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -114,34 +114,55 @@ class Home extends Component {
                     <Row>
                         <Col md={4}>
                             <Card className='home_card card_1'>
+                            <div className='card_content_div'>
                                 <div className='card_content'>
+                                    <div className="card_heading">
+                                        <h4>Placeholder card</h4>
+                                    </div>
+                                    <div className='card_text'>
+                                    Lorem ipsum dolor sit amet, consectetur adipisci elit, 
+                                    sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrum exercitationem ullam 
+                                    corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
+                                    consequatur.
+                                    </div>
+                                </div>
+                            </div>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card className='home_card card_2'>
+                            <div className='card_content_div'>
+                                <div className='card_content'>
+                                    <div className="card_heading">
+                                        <h4>Placeholder card</h4>
+                                    </div>
+                                    <div className='card_text'>
+                                    Lorem ipsum dolor sit amet, consectetur adipisci elit, 
+                                    sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrum exercitationem ullam 
+                                    corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
+                                    consequatur.
+                                    </div>
+                                </div>
+                            </div>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card className='home_card card_3'>
+                            <div className='card_content_div'>
+                            <div className='card_content'>
+                                <div className="card_heading">
+                                    <h4>Placeholder card</h4>
+                                </div>
+                                <div className='card_text'>
                                 Lorem ipsum dolor sit amet, consectetur adipisci elit, 
                                 sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
                                 Ut enim ad minim veniam, quis nostrum exercitationem ullam 
                                 corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
                                 consequatur.
                                 </div>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card className='home_card card_2'>
-                            <div className='card_content'>
-                                Lorem ipsum dolor sit amet, consectetur adipisci elit, 
-                                sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
-                                Ut enim ad minim veniam, quis nostrum exercitationem ullam 
-                                corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
-                                consequatur.
                             </div>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card className='home_card card_3'>
-                            <div className='card_content'>
-                                Lorem ipsum dolor sit amet, consectetur adipisci elit, 
-                                sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
-                                Ut enim ad minim veniam, quis nostrum exercitationem ullam 
-                                corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
-                                consequatur.
                             </div>
                             </Card>
                         </Col>
