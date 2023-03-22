71e83e849696c7ac2dab238d61e3ca08f9f8c021
priyanshukaushal <priyanshu.kaushal2001@gmail.com>
2023-03-23 01:31:47 +0530

    firebase config file added

---
+++ src/config/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
