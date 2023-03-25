4d1309c9c4554d351a799138f1dce1bd0ca980f2
priyanshukaushal <priyanshu.kaushal2001@gmail.com>
2023-03-26 01:39:35 +0530

    google login added

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,14 +1,12 @@
-import { initializeApp } from "firebase/app";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
+
