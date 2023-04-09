a6bcf7b1de0b057e0d09efb656ddc33827fe3395
Aseem Mangla <aseemmanglap@gmail.com>
2023-04-09 17:58:56 +0530

    about acm

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,14 +11,7 @@ class About extends Component {
                     <h2 className='about_section_heading'>Who are we?</h2>
                     <Row className='about_section_content'>
                         <Col className='text_content'>
-                            Lorem ipsum dolor sit amet, consectetur adipisci elit,
-                            sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrum exercitationem ullam
-                            corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-                            consequatur. Quis aute iure reprehenderit in voluptate velit
-                            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                            obcaecat cupiditat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
+                        The Association for Computing Machinery (ACM) is an international learned society for computing. It was founded in 1947 and is the world’s largest scientific and educational computing society. The ACM is an umbrella organization for academic and scholarly interests in computer science. PEC ACM CSS is a community of learners and leaders who teach each other to supplement the development of the members’ college education, specifically with regards to Computer Science. The vision of PEC ACM is to provide students exposure to the wide array of topics that fall under the umbrella of the field of computer science.
                         </Col>
                         <Col className='about_section_content_img' >
                             <Image src={img1} alt='ACM Groups' fluid />
